15ccb4d254a84bf51a3f39d3e4c3a425ecbd91a3
Nick Felker <fleker@google.com>
2023-10-13 15:40:02 -0400

    Add Firebase config

--- client/src/app/service/firebase.config.ts
+++ client/src/app/service/firebase.config.ts
@@ -1,9 +1,9 @@
 export default {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDJUFyaMQ8pc7ep9nTzCGZNVSPlBDNfAZU",
+  authDomain: "pokemon-as-a-service.firebaseapp.com",
+  projectId: "pokemon-as-a-service",
+  storageBucket: "pokemon-as-a-service.appspot.com",
+  messagingSenderId: "131857406111",
+  appId: "1:131857406111:web:f0984fc908f7fe45106aaa",
+  measurementId: "G-4KR9YZ41Z4"
 }
