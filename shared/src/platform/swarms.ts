2ef514477136ed0f122b6b34ac10d388336a277d
Nick Felker <fleker@google.com>
2024-03-19 13:09:43 -0400

    Update swarms

--- shared/src/platform/swarms.ts
+++ shared/src/platform/swarms.ts
@@ -6,24 +6,24 @@ import * as P from '../gen/type-pokemon'
 
 /**
  * ```@markdown
-potw-001 Bulbasaur (4)
-potw-004 Charmander (2)
-potw-052 Meowth (2)
-potw-025 Pikachu (2)
-potw-021 Hippopotas (1)
-potw-837 Rolycoly (1)
-potw-868 Milcery (1)
-potw-013 Luvdisc (1)
-33 votes in total
+potw-077-galarian Ponyta (4)
+potw-025 Pikachu (3)
+potw-133 Eevee (2)
+potw-024 Arbok (2)
+potw-001 Bulbasaur (2)
+potw-037-alolan Vulpix (1)
+potw-866 Mr. Rime (1)
+potw-050 Diglett (1)
+36 votes in total
  * ```
  */
 export const Swarms: Record<RegionType, BadgeId> = {
-  'Africa / Middle East': P.Hippopotas,
+  'Africa / Middle East': P.Bulbasaur,
   'Asia': P.Pikachu,
-  'Australia / New Zealand': P.Rolycoly,
-  'Mediterranean': P.Milcery,
-  'North America': P.Bulbasaur,
-  "North Europe": P.Charmander,
-  "Pacific Islands": P.Luvdisc,
-  "South America": P.Meowth,
+  'Australia / New Zealand': P.Ekans,
+  'Mediterranean': P.Eevee,
+  'North America': P.Mime_Jr,
+  "North Europe": Potw(P.Ponyta, {form: 'galarian'}),
+  "Pacific Islands": Potw(P.Vulpix, {form: 'alolan'}),
+  "South America": P.Diglett,
 }
