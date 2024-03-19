bd78c46c283e8902e60d7e37b9b50fbc5a599f91
Nick Felker <fleker@google.com>
2024-03-19 13:01:51 -0400

    Add new training cards

--- client/src/app/pages/page-movetutor/page-movetutor.component.ts
+++ client/src/app/pages/page-movetutor/page-movetutor.component.ts
@@ -4,7 +4,7 @@ import { Component} from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { PokemonDialogComponent } from 'src/app/dialogs/pokemon-dialog/pokemon-dialog.component';
 import { FirebaseService } from 'src/app/service/firebase.service';
-import { Badge, MATCH_FILTER, MATCH_REQS, Nature, NatureDescription } from '../../../../../shared/src/badge3';
+import { Badge, Nature, NatureDescription } from '../../../../../shared/src/badge3';
 import { ITEMS, ItemId } from '../../../../../shared/src/items-list';
 import { get } from '../../../../../shared/src/pokemon';
 import { PokemonId } from '../../../../../shared/src/pokemon/types';
@@ -12,11 +12,14 @@ import {getEligiblePokemonForMove, getVariantForMove} from '../../../../../share
 import { MoveId } from '../../../../../shared/src/gen/type-move-meta';
 import { F } from '../../../../../shared/src/server-types';
 import { EvolutionComponent } from 'src/app/ui/evolution/evolution.component';
+import { EngagementService } from 'src/app/engagement.service';
 
 enum Mode {
   TM = 1,
   Mint = 2,
   Tutor3 = 3,
+  Mushroom = 4,
+  Honey = 5,
   Tutor6 = 6,
 }
 
@@ -45,8 +48,18 @@ export class PageMovetutorComponent implements AfterViewInit, OnDestroy {
     nature: Nature
     description: string
   }[]
+  teraShards?: {
+    key: ItemId
+    label: string
+    count: number
+    description: string
+  }[]
   heartScales: number = -1
   armorite: number = -1
+  maxMushroom = 0
+  maxHoney = 0
+  teraOrb = false
+
   selection?: PokemonId
   selectionLabel?: string
   selectionVar?: number
@@ -64,6 +77,7 @@ export class PageMovetutorComponent implements AfterViewInit, OnDestroy {
   constructor(
     private firebase: FirebaseService,
     private snackbar: MatSnackBar,
+    private engagement: EngagementService,
   ) { }
 
   ngAfterViewInit(): void {
@@ -71,6 +85,8 @@ export class PageMovetutorComponent implements AfterViewInit, OnDestroy {
       if (user) {
         this.heartScales = user.items.heartscale || 0
         this.armorite = user.items.armorite || 0
+        this.maxMushroom = user.items.maxmushroom || 0
+        this.maxHoney = user.items.maxhoney || 0
         this.tms = Object.keys(user.items).sort()
           .filter(item => ITEMS[item]?.category === 'tms')
           .filter(item => user.items[item] > 0)
@@ -89,6 +105,15 @@ export class PageMovetutorComponent implements AfterViewInit, OnDestroy {
           })).map(item => ({
             ...item,
             description: NatureDescription[item.nature],
+          }))
+        this.teraOrb = user.items.teraorb > 0 || this.engagement.isNextUi
+        this.teraShards = Object.keys(ITEMS).sort()
+          .filter(item => ITEMS[item].category === 'terashard')
+          .map(item => ({
+            key: item as ItemId,
+            label: ITEMS[item].label,
+            count: user.items[item] ?? 0,
+            description: ITEMS[item].description,
           }))
         console.log(this.mints)
         if (user.ldap === 'fleker') {
@@ -169,6 +194,21 @@ export class PageMovetutorComponent implements AfterViewInit, OnDestroy {
     this.pokemon.open()
   }
 
+  useGMax(item: 'mushroom' | 'honey') {
+    if (item === 'mushroom') {
+      this.mode = Mode.Mushroom
+    } else if (item === 'honey') {
+      this.mode = Mode.Honey
+    }
+    this.pokemon.usePredicate(x => {
+      const badge = new Badge(x)
+      const str = badge.toLegacyString()
+      const dbPkmn = get(str)!
+      return dbPkmn.gmax !== undefined
+    })
+    this.pokemon.open()
+  }
+
   close() {
     this.dialog!.nativeElement.close()
   }
@@ -189,6 +229,12 @@ export class PageMovetutorComponent implements AfterViewInit, OnDestroy {
           } else if (this.mode === Mode.Mint) {
             await this.firebase.exec<F.UseItem.Req, F.UseItem.Res>('use_item', { target: this.selection, item: this.mint })
             this.snackbar.open(`${this.selectionLabel} consumed the mint!`, '', {duration: 3000})
+          } else if (this.mode === Mode.Mushroom) {
+            await this.firebase.exec<F.TrainPokemon.Req, F.TrainPokemon.Res>('use_item', { species: this.selection, item: 'maxmushroom' })
+            this.snackbar.open(`${this.selectionLabel} consumed the max soup!`, '', {duration: 3000})
+          } else if (this.mode === Mode.Honey) {
+            await this.firebase.exec<F.TrainPokemon.Req, F.TrainPokemon.Res>('use_item', { species: this.selection, item: 'maxhoney' })
+            this.snackbar.open(`${this.selectionLabel} consumed the max soup!`, '', {duration: 3000})
           }
           this.firebase.refreshUser()
         } catch (e: any) {
