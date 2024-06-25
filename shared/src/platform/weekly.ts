a1f30ee775e945c050ee29035359023593815067
Nick Felker <fleker@google.com>
2024-06-25 16:38:50 -0400

    Update raid bosses for terrain and region

--- shared/src/platform/weekly.ts
+++ shared/src/platform/weekly.ts
@@ -1,5 +1,5 @@
 import * as P from '../gen/type-pokemon'
 import * as I from '../gen/type-pokemon-ids'
 
-export const weekly = P.Mawile
-export const weeklyId = I.Mawile
+export const weekly = P.Aron
+export const weeklyId = I.Aron
