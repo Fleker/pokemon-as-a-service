309235c8cb70377ea14cd32a2eb0402ad59f50e9
Nick Felker <fleker@google.com>
2024-06-25 16:21:53 -0400

    Update swarms

--- shared/src/platform/swarms.ts
+++ shared/src/platform/swarms.ts
@@ -7,17 +7,17 @@ import * as P from '../gen/type-pokemon'
 /**
  * ```@markdown
 potw-885 Dreepy (2)
+potw-077 Ponyta (2)
 potw-935 Charcadet (1)
 potw-194-paldean Wooper (1)
 potw-019-alolan Rattata (1)
 potw-213 Shuckle (1)
 potw-767 Wimpod (1)
 potw-023 Ekans (1)
-potw-531 Audino (1)
 ---
+potw-531 Audino (1)
 potw-867 Runerigus (1)
 potw-855 Polteageist (1)
-potw-077 Ponyta (1)
 potw-024 Arbok (1)
 potw-117 Seadra (1)
 potw-116 Horsea (1)
@@ -28,17 +28,29 @@ potw-179 Mareep (1)
 potw-129-apricot_stripes Magikarp (1)
 potw-327 Spinda (1)
 potw-447 Riolu (1)
+potw-404 Luxio (1)
+potw-236 Tyrogue (1)
+potw-351-sunny Castform (1)
+potw-060 Poliwag (1)
+potw-132 Ditto (1)
+potw-302 Sableye (1)
+potw-004 Charmander (1)
+potw-440 Happiny (1)
+potw-363 Spheal (1)
+potw-032 Nidoran♂ (1)
+potw-240 Magby (1)
+34 votes in total
  * ```
  */
 export const Swarms: Record<RegionType, BadgeId> = {
-  'Africa / Middle East': P.Dreepy,
-  'Asia': Potw(P.Rattata, {form: 'alolan'}),
-  'Australia / New Zealand': Potw(P.Wooper, {form: 'paldean'}),
-  'Mediterranean': P.Shuckle,
-  'North America': P.Charcadet,
-  "North Europe": P.Ekans,
-  "Pacific Islands": P.Wimpod,
-  "South America": P.Audino,
+  'Africa / Middle East': Potw(P.Yamask, {form:'galarian'}),
+  'Asia': P.Spinda,
+  'Australia / New Zealand': P.Sinistea,
+  'Mediterranean': P.Horsea,
+  'North America': P.Sableye,
+  "North Europe": P.Growlithe,
+  "Pacific Islands": P.Mareep,
+  "South America": P.Dunsparce,
 }
 
 /**
@@ -54,14 +66,14 @@ export const Swarms: Record<RegionType, BadgeId> = {
  * minutes before and after half-past a particular hour.
  */
 export const MassiveOutbreaks: Record<WeatherType, BadgeId> = {
-  Cloudy: P.Spinda,
-  'Diamond Dust': P.Polteageist,
-  Snow: P.Polteageist,
-  Fog: P.Horsea,
-  'Heat Wave': P.Growlithe,
-  Rain: Potw(P.Magikarp, {form: 'apricot_stripes'}),
-  Sandstorm: Potw(P.Yamask, {form: 'galarian'}),
-  Thunderstorm: P.Mareep,
-  Windy: P.Dunsparce,
+  Cloudy: P.Riolu,
+  'Diamond Dust': P.Happiny,
+  Snow: P.Happiny,
+  Fog: P.Poliwag,
+  'Heat Wave': P.Magby,
+  Rain: P.Buizel,
+  Sandstorm: P.Nidoran_M,
+  Thunderstorm: P.Spheal,
+  Windy: P.Tyrogue,
   Sunny: P.Ditto // No-op
 }
